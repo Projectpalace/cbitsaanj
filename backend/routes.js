108cc45388e7f80e5154378539fbe4a525c4369b
vsreddyh <shouryanreddyh@gmail.com>
2024-04-26 00:32:59 +0530

    back ready

--- backend/routes.js
+++ backend/routes.js
@@ -1,4 +1,4 @@
 const router = require('express').Router();
-const {welcome} = require('.controllers/functions')
+const {welcome} = require('./controllers/functions')
 router.post('/welcome',welcome)
 module.exports = router
