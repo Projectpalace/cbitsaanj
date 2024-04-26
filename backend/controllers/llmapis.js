50cb0f3e54ae80aff947071c3f99c17f4d803258
Naga sai praneeth <123751071+NagasaiPraneeth@users.noreply.github.com>
2024-04-26 20:49:30 +0530

    .

--- backend/controllers/llmapis.js
+++ backend/controllers/llmapis.js
@@ -1,84 +1,127 @@
-const express=require('express')
-const multer=require('multer')
+const mongoose = require('mongoose');
 const fs = require('fs');
-const pdfParse = require('pdf-parse');const mongoose = require('mongoose');
-const { MongoClient, GridFSBucket } = require('mongodb');
-let matter="";
+const multer = require('multer');
+const express = require('express');
+const app = express();
+const pdfParse = require('pdf-parse');
+app.use(express.json());
+const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
 
-const app=express();
 app.use(express.json());
 
-const upload=multer({dest:'uploads/'})
+const upload = multer({ dest: 'uploads/' });
+
+mongoose.connect('mongodb://user_42bch7t68:p42bch7t68@ocdb.app:5050/db_42bch7t68?retryWrites=true&w=majority');
+const db = mongoose.connection;
+
+// Check connection
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Check for DB errors
+db.on('error', err => {
+  console.error(err);
+});
+
+const pdfSchema = new mongoose.Schema({
+  name: String,
+  content: Buffer
+});
 
 
-const uploadFile=(req, res) =>{
-  const upload = multer({dest:'uploads/'}).single('pdf');
-  upload(req, res, async function (err) {
-    if (err) {
-      return res.status(400).json({ error: err.message });
-    }
-    const pdfBuffer = fs.readFileSync(req.file.path);
-    let matter = "";
-    pdfParse(pdfBuffer).then(async data => {
-      matter = data.text;
+// Create a model based on the schema
+const PdfModel = mongoose.model('Pdf', pdfSchema);
 
-      const {
-        GoogleGenerativeAI,
-        HarmCategory,
-        HarmBlockThreshold,
-      } = require("@google/generative-ai");
+const reportSchema = new mongoose.Schema({
+  patient: mongoose.Schema.Types.ObjectId,
+  doctor: mongoose.Schema.Types.ObjectId,
+  File: { type: mongoose.Schema.Types.ObjectId },
+  dateopen: Date,
+  symptoms: [String],
+  fileclosed: Boolean,
+  suggtreats: [String],
+  summary: [String],
+  measures: [String],
+  preventive_measures: [String],
+  doctornotes: { type: mongoose.Schema.Types.ObjectId },
+  Medications: { type: mongoose.Schema.Types.ObjectId }
+});
 
-      const MODEL_NAME = "gemini-1.0-pro";
-      const API_KEY = "AIzaSyBCrBhLm4UM0Gj5V8LwQlXJyks9TyE3FG8";
+const ReportModel = mongoose.model('Report', reportSchema);
 
-      const genAI = new GoogleGenerativeAI(API_KEY);
-      const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+const uploadFile = async (req, res) => {
+  try {
+    const pdfBuffer = req.file.buffer; // Access the file buffer directly
 
-      const generationConfig = {
-        temperature: 0,
-        topK: 1,
-        topP: 1,
-        maxOutputTokens: 2048,
-      };
-
-      const safetySettings = [
-        {
-          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-          threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-      ];
-
-      const parts = [
-        {
-          text:
-            "\nthe patient will give the laboratory test report in the pdf parser text . if the report does not contain any health parameter values or specific medical information that would allow for a diagnosis must give a response as \"invalid health report\" and don't print anything. if it has medical information you should analyze the data,health parameters and values and predict any disease that the user have and give him/her suggestion to improve health based on the report. .don't give any health parameter values.if the patient have any serious condition mention them in the first .the format of the response should be contain 3 paragraphs the first paragraph describes about  the  disease they have and second paragraph should be about risk of diseases they may face in future and the third paragraph should be suggestions and tips to improve their health based on the health report. the health suggestions should be precise. give the headings to the three paragraphs. \n\. the parsed pdf: " +
-            matter,
-        },
-      ];
-
-      const result = await model.generateContent({
-        contents: [{ role: "user", parts }],
-        generationConfig,
-        safetySettings,
-      });
-
-      const response = result.response;
-      res.send({ message: response.text() });
+    const newPdf = new PdfModel({
+      name: req.file.originalname,
+      content: pdfBuffer
     });
-  });
-}
+    await newPdf.save();
+
+    const newReport = new ReportModel({
+      patient: null,
+      doctor: null,
+      File: newPdf._id,
+      dateopen: new Date(),
+      symptoms: [],
+      fileclosed: false,
+      suggtreats: [],
+      summary: [],
+      measures: [],
+      preventive_measures: [],
+      doctornotes: null,
+      Medications: null
+    });
+    await newReport.save();
+
+    console.log("PDF uploaded and saved successfully");
+
+    // Read PDF buffer and parse text
+    const matter = await pdfParse(pdfBuffer);
+    const {
+      GoogleGenerativeAI,
+      HarmCategory,
+      HarmBlockThreshold,
+    } = require("@google/generative-ai");
+
+
+    const genAI = new GoogleGenerativeAI("AIzaSyBCrBhLm4UM0Gj5V8LwQlXJyks9TyE3FG8"); // Replace API_KEY with your actual API key
+    const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
+
+    const generationConfig = {
+      temperature: 0,
+      topK: 1,
+      topP: 1,
+      maxOutputTokens: 2048,
+    };
+
+    const safetySettings = [
+      { category: HarmCategory.HARM_CATEGORY_HARASSMENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+      { category: HarmCategory.HARM_CATEGORY_HATE_SPEECH, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+      { category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+      { category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT, threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE },
+    ];
+
+    const parts = [{
+      text: "\nthe patient will give the laboratory test report in the pdf parser text . if the report does not contain any health parameter values or specific medical information that would allow for a diagnosis must give a response as \"invalid health report\" and don't print anything. if it has medical information you should analyze the data,health parameters and values and predict any disease that the user have and give him/her suggestion to improve health based on the report. .don't give any health parameter values.if the patient have any serious condition mention them in the first .the format of the response should be contain 3 paragraphs the first paragraph describes about  the  disease they have and second paragraph should be about risk of diseases they may face in future and the third paragraph should be suggestions and tips to improve their health based on the health report. the health suggestions should be precise. give the headings to the three paragraphs. \n\. the parsed pdf: " + matter.text,}];
+
+    const result = await model.generateContent({
+      contents: [{ role: "user", parts }],
+      generationConfig,
+      safetySettings,
+    });
+
+    const response = result.response;
+    console.log(response.text());
+    res.status(200).json({ message: response.text() });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error.' });
+  }
+};
+
 
 const formatFile= (req, res) => {
   const matter=req.body.message;
