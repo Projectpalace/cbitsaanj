557583ea008d7568681c518e2fca398302ddcbfa
Nithin Chowdary <nithinchowdary2354@gmail.com>
2024-04-27 04:45:18 +0530

    3

--- frontend/src/components/update.jsx
+++ frontend/src/components/update.jsx
@@ -20,7 +20,7 @@ const FileUpload = () => {
     
 
     // First axios request to /api/upload
-    axios.post('/upload', formData)
+    axios.post('/api/upload', formData)
       
       .then(response => {
         // Store the chat response in state
