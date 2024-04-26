e0c6f20a02bbb1a109a5afc9e1a904548a5ff691
Nithin Chowdary <nithinchowdary2354@gmail.com>
2024-04-26 22:56:38 +0530

    Revert "2nd"
    This reverts commit 3e9606dd5206ab5441c71d1c69d71f02211018b0.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Main from './components/main';
 import Home from './components/Home';
 import Caretaker from './components/Caretaker';
 import FileUpload from './components/update';
-import InputReport from './components/import_reports';
 
 export default function App() {
     return (
@@ -14,7 +13,7 @@ export default function App() {
                     <Route path='/' element={<Home/>} />
                     <Route path='/doctor' element={<Main/>}/>
                     <Route path='/ct' element={<Caretaker/>}/>
-                    <Route path='/upload' element={<InputReport/>}/>
+                    <Route path='/upload' element={<FileUpload/>}/>
                 </Routes>
             </Router>
         </div>
