145796f142b9e4d9eff9fbba77be166bb6fa0645
vsreddyh <shouryanreddyh@gmail.com>
2024-06-11 17:19:55 +0530

    fixed build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import Main from './components/main';
 import Home from './components/Home';
 import Caretaker from './components/Caretaker';
 import FileUpload from './components/update';
-import SpeechToText from './components/chatbot';
-import Recording from './components/Recording';
 export default function App() {
     return (
         <div className='App'>
@@ -15,8 +13,6 @@ export default function App() {
                     <Route path='/doctor' element={<Main/>}/>
                     <Route path='/ct' element={<Caretaker/>}/>
                     <Route path='/upload' element={<FileUpload/>}/>
-                    <Route path='/cb' element={<SpeechToText/>}/>
-                    <Route path='/recording' element={<Recording/>}/>
                 </Routes>
             </Router>
         </div>
